46ca621010a5cd7ee02c302e48d7614b74d34969
melwin <114172287+itzTedx@users.noreply.github.com>
2025-08-29 11:47:36 +0400

    refactor(categories): improve layout and functionality of CategoriesPage with ProductCard integration

--- src/app/(root)/categories/page.tsx
+++ src/app/(root)/categories/page.tsx
@@ -3,21 +3,30 @@ import Image from "next/image";
 import { SectionHeader } from "@/components/layout/section-header";
 
 import { getCategories } from "@/modules/categories/actions/query";
+import { ProductCard } from "@/modules/product/components";
 
 export default async function CategoriesPage() {
   const categories = await getCategories();
 
   return (
-    <main className="container relative space-y-12 pb-8 sm:pb-12 md:space-y-16 md:pb-16 lg:pb-20">
-      <SectionHeader description="Browse our categories" hasButton={false} title="Categories" />
-      <div className="grid gap-3 sm:gap-4 md:gap-6">
+    <main className="container relative space-y-12 py-8 sm:pb-12 md:space-y-16 md:pb-16 lg:pb-20">
+      <div className="grid gap-3 sm:gap-4 md:gap-9">
         {categories.map((category) => {
           const thumbnail = category.images.find((image) => image.type === "thumbnail")?.media;
           return (
-            <div key={category.id}>
-              {thumbnail && thumbnail.url && <Image alt={category.name} height={100} src={thumbnail.url} width={100} />}
-              {category.name}
-            </div>
+            <section id={category.name} key={category.id}>
+              {thumbnail && thumbnail.url && (
+                <div className="relative h-56 w-full overflow-hidden rounded-xl">
+                  <Image alt={category.name} className="object-cover" fill src={thumbnail.url} />
+                </div>
+              )}
+              <SectionHeader description={category.description} hasButton={false} title={category.name} />
+              <article className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                {category.products.map((product) => (
+                  <ProductCard data={product} key={product.id} />
+                ))}
+              </article>
+            </section>
           );
         })}
       </div>
